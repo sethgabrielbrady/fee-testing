7c88598d6ec71ac86c77bf9ed7785da58efb2482
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-16 00:01:16 -0400

    cleaned up a bit

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -1,101 +1,92 @@
-//
-// Write tests for the two functions in the window.calc namespace...
-// The description of the functions are in the source code, read them!
-// Ensure that you are testing not just the success conditions,
-// but also the failures! For example, ensure that a function acts as
-// expected both with good arguments and bad arguments!
-//
-//
-
 
 (function() {
     'use strict';
     let expect = window.chai.expect;
-      describe('math tests', function(){
+    describe('math tests', function() {
 
-        describe ('sum function', function(){
+        describe('sum function', function() {
 
-            it('should return a number when an argument is passed', function(){
-            let result = window.calc.sum([1, 1]);
-            expect(result).to.be.a('number');
-            expect(result).to.equal(2);
+            it('should return a number when an argument is passed', function() {
+                let result = window.calc.sum([1, 1, 1]);
+                expect(result).to.be.a('number');
+                expect(result).to.equal(3);
             });
 
-            it('should return a number when only one number is passed', function(){
-            let result = window.calc.sum([1]);
-            expect(result).to.be.a('number');
-            expect(result).to.equal(1);
+            it('should return a number when only one number is passed', function() {
+                let result = window.calc.sum([1]);
+                expect(result).to.be.a('number');
+                expect(result).to.equal(1);
             });
 
-            it('should return 0 when no number or an empty array is passed', function(){
-            let result = window.calc.sum([]);
-            expect(result).to.be.a('number');
-            expect(result).to.equal(0);
+            it('should return 0 when no number or an empty array is passed', function() {
+                let result = window.calc.sum([]);
+                expect(result).to.be.a('number');
+                expect(result).to.equal(0);
             });
 
-            it('should return 0 something other than array is passes', function(){
-            let result = window.calc.sum('3');
-            expect(result).to.be.a('number');
-            expect(result).to.equal(0);
+            it('should return 0 something other than array is passed', function() {
+                let result = window.calc.sum('3');
+                expect(result).to.be.a('number');
+                expect(result).to.equal(0);
             });
 
-            it('should return a number when an array numbers as strings is passed ', function(){
-            let result = window.calc.sum(['1', '1']);
-            expect(result).to.be.a('number');
-            expect(result).to.equal(2);
+            it('should return a number when an array containing numbers as strings is passed ', function() {
+                let result = window.calc.sum(['1', '1']);
+                expect(result).to.be.a('number');
+                expect(result).to.equal(2);
             });
 
-            it('should return NaN when an array of something other than numbers is passed', function(){
-            let result = window.calc.sum(['green', true]);
-            expect(result).to.be.NaN;
+            it('should return NaN when an array of something other than numbers is passed', function() {
+                let result = window.calc.sum(['green', true]);
+                expect(result).to.be.NaN;
             });
 
-          });
-
-
-
-          describe('factorial test', function(){
-
-            it('should return 1 when 1 is passed', function(){
-            let result = window.calc.factorial(1);
-            expect(result).to.be.a('number');
-            expect(result).to.equal(1);
-            });
-
-
-            it('should return 0 when something other than a number is passed', function(){
-            let result = window.calc.factorial(['number', 0]);
-            expect(result).to.be.a('number');
-            expect(result).to.equal(0);
-            });
-
-            it('should return 0 when nothing is passed', function(){
-            let result = window.calc.factorial();
-            expect(result).to.be.a('number');
-            expect(result).to.equal(0);
-            });
-
-            it('should return a number as a string as a number', function(){
-            let result = window.calc.factorial('3');
-            expect(result).to.be.a('number');
-            expect(result).to.equal(6);
-            });
-
-            it('should return the first number if more than one number is passed', function(){
-            let result = window.calc.factorial(1, 2);
-            expect(result).to.be.a('number');
-            expect(result).to.equal(1);
-            });
-
-
-            it('should return NaN when NaN is passed to it', function(){
-            let result = window.calc.factorial(NaN);
-            expect(result).to.be.NaN;
-            });
-
-
-          });
         });
+
+
+
+        describe('factorial test', function() {
+
+            it('should return 1 when 1 is passed', function() {
+                let result = window.calc.factorial(1);
+                expect(result).to.be.a('number');
+                expect(result).to.equal(1);
+            });
+
+
+            it('should return 0 when something other than a number is passed', function() {
+                let result = window.calc.factorial(['number', 0]);
+                expect(result).to.be.a('number');
+                expect(result).to.equal(0);
+            });
+
+            it('should return 0 when nothing is passed', function() {
+                let result = window.calc.factorial();
+                expect(result).to.be.a('number');
+                expect(result).to.equal(0);
+            });
+
+            it('should return a number as a string as a number', function() {
+                let result = window.calc.factorial('3');
+                expect(result).to.be.a('number');
+                expect(result).to.equal(6);
+            });
+
+            it('should return the first number if more than one number is passed', function() {
+                let result = window.calc.factorial(1, 2);
+                expect(result).to.be.a('number');
+                expect(result).to.equal(1);
+            });
+
+
+            it('should return NaN when NaN is passed to it', function() {
+                let result = window.calc.factorial(NaN);
+                expect(result).to.be.NaN;
+            });
+
+
+        });
+    });
 
 })();
 
